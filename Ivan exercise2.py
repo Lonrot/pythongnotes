9a27c9c3bdf998dfcfd80123c11fd7a8cdeaa0cc
Ivanhuicc <Ivan.Hui@charitycommission.gov.uk>
2025-08-07 19:46:47 +0100

    Add files via upload
    I've tried to use elif but for some reason it's not printing the correct outcome, its printing both add book and remove book :(

---
+++ Ivan exercise2.py
@@ -0,0 +1,14 @@
+hello = 'hello'
+name = input('What"s your name')
+greeting = hello + " " + name
+print(greeting)
+
+user_choice = input('Do you choose book, music, game')
+if user_choice == 'book':
+    input('Do you want to add, or remove?')
+if 'add':
+    print('add book')
+elif 'remove':
+    print('remove book')
+
+    
